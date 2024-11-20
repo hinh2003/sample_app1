de89191913277f2df97742a306663ab0176e4669
hinh2003 <vanhinh2003@gmail.com>
2024-11-20 14:14:40 +0700

    Implement basic login

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   # get "static_pages/home" ,to:"static_pages#home"
   get "/help" , to:"static_pages#help", as:'helf'
   get "/about" , to:"static_pages#about"
   get "/contact" , to:"static_pages#contact"
   get "/signup" , to:"users#new"
+  get "/login" , to:"sessions#new"
+  post "/login" , to:"sessions#create"
+  get "/logout" , to:"sessions#destroy"
   resources :users
 
-  # get 'static_pages/help'
-  # get 'static_pages/about'
-  # get 'static_pages/contact'
-  # get 'static_pages/home'
   root 'static_pages#home'
 end
