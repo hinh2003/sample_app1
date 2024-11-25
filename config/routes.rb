6aaf0e4843050d2d211640799c7d82871d7c3214
hinh2003 <vanhinh2003@gmail.com>
2024-11-25 14:58:39 +0700

    refs #14 fixlint

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,24 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # get "static_pages/home" ,to:"static_pages#home"
-  get "/help" , to:"static_pages#help", as:'helf'
-  get "/about" , to:"static_pages#about"
-  get "/contact" , to:"static_pages#contact"
-  get "/signup" , to:"users#new"
-  get "/login" , to:"sessions#new"
-  post "/login" , to:"sessions#create"
-  get "/logout" , to:"sessions#destroy"
+  get '/help', to: 'static_pages#help', as: 'helf'
+  get '/about', to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
+  get '/signup', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  get '/logout', to: 'sessions#destroy'
+  post '/login', to: 'sessions#create'
+
   resources :users do
     member do
       get :following, :followers
     end
   end
   resources :account_activations, only: [:edit]
-  resources :password_resets, only: [:new, :create, :edit, :update]
-  resources :microposts, only: [:create, :destroy]
-  resources :relationships, only: [:create, :destroy]
+  resources :password_resets, only: %i[new create edit update]
+  resources :microposts, only: %i[create destroy]
+  resources :relationships, only: %i[create destroy]
 
   root 'static_pages#home'
 end
