8b320f55108732cdabfa62af3c779a6c3efbdfd6
hinh2003 <vanhinh2003@gmail.com>
2024-11-25 14:58:39 +0700

    fix lint

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,75 +1,92 @@
+# frozen_string_literal: true
+
+# The User model represents a user in the application.
+# It is responsible for handling user authentication, registration,
+# profile management, and other user-related functionality.
+# This model is associated with several other models like `Post`, `Relationship`,
+# and more. It uses features like secure password authentication and
+# token generation for password resets.
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update, :destroy,
-                                        :following, :followers]
-  before_action :correct_user, only: [:edit, :update]
+  before_action :logged_in_user, only: %i[index edit update destroy
+                                          following followers]
+  before_action :correct_user, only: %i[edit update]
   before_action :admin_user, only: :destroy
   def following
-    @title = "Following"
+    @title = 'Following'
     @user = User.find(params[:id])
     @users = @user.following.paginate(page: params[:page])
     render 'show_follow'
   end
+
   def followers
-    @title = "Followers"
+    @title = 'Followers'
     @user = User.find(params[:id])
     @users = @user.followers.paginate(page: params[:page])
     render 'show_follow'
   end
+
   def new
     @user = User.new
   end
+
   def index
     @users = User.where(activated: true).paginate(page: params[:page])
   end
+
   def show
     @user = User.find(params[:id])
     @microposts = @user.microposts.paginate(page: params[:page])
   end
+
   def create
     @user = User.new(user_params)
     if @user.save
       UserMailer.account_activation(@user).deliver_now
-      flash[:info] = "Please check your email to activate your account."
+      flash[:info] = 'Please check your email to activate your account.'
       redirect_to root_url
     else
       render 'new'
     end
   end
+
   def edit
     @user = User.find(params[:id])
   end
+
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-      flash[:success] = "Profile updated"
+      flash[:success] = 'Profile updated'
       redirect_to @user
     else
       render 'edit'
     end
   end
+
   def logged_in_user
-    unless logged_in?
-      store_location
-      flash[:danger] = "Please log in."
-      redirect_to login_url
-    end
+    return if logged_in?
+
+    store_location
+    flash[:danger] = 'Please log in.'
+    redirect_to login_url
   end
+
   def correct_user
     @user = User.find(params[:id])
     redirect_to(root_url) unless current_user?(@user)
   end
+
   def destroy
     User.find(params[:id]).destroy
-    flash[:success] = "User deleted"
+    flash[:success] = 'User deleted'
     redirect_to users_url
   end
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
+
   def admin_user
     redirect_to(root_url) unless current_user.admin?
   end
-
-
 end
--- app/helpers/microposts_helper.rb
+++ app/helpers/microposts_helper.rb
@@ -1,2 +1,10 @@
+# frozen_string_literal: true
+
+# The User model represents a user in the application.
+# It is responsible for handling user authentication, registration,
+# profile management, and other user-related functionality.
+# This model is associated with several other models like `Post`, `Relationship`,
+# and more. It uses features like secure password authentication and
+# token generation for password resets.
 module MicropostsHelper
 end
