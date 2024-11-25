4c4ab0e34ffba495d09ba1eab64d8a9ce8c3ca2f
hinh2003 <vanhinh2003@gmail.com>
2024-11-25 14:58:39 +0700

    refs #14 fixlint

--- db/migrate/20241120072155_add_remember_digest_to_users.rb
+++ db/migrate/20241120072155_add_remember_digest_to_users.rb
@@ -1,3 +1,9 @@
+# frozen_string_literal: true
+
+# Migration to create the relationships table for users following each other.
+# This table holds the follower and followed user IDs, which represent a
+# many-to-many relationship between users. It allows tracking of who follows
+# whom in the application.
 class AddRememberDigestToUsers < ActiveRecord::Migration[6.1]
   def change
     add_column :users, :remember_digest, :string
